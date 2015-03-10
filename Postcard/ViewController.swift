bb6e88b2f83a52efa7725e0fe48e290905f9f684
Son Nguyen <sonnguyen@Sons-Air.lan>
2015-03-10 18:51:05 -0400

    added a comment to test commits

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -24,6 +24,7 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        // adding comment to test commit.
     }
 
     @IBAction func sendMailButtonPressed(sender: UIButton) {
